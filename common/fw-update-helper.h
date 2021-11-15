6feb5d855eed33afadeaa4b305db75fe88801882
doronhi <doron_hirshberg@hotmail.com>
2021-11-15 09:46:29 +0200

    Rebase from 'debian/galactic/librealsense2'

--- common/fw-update-helper.h
+++ common/fw-update-helper.h
@@ -10,12 +10,13 @@ namespace rs2
 {
     class viewer_model;
 
-    int parse_product_line(std::string id);
-    std::string get_available_firmware_version(int product_line);
-    std::map<int, std::vector<uint8_t>> create_default_fw_table();
+    int parse_product_line(const std::string& product_line);
+    std::string get_available_firmware_version(int product_line, const std::string& pid);
+
+    std::vector<uint8_t> get_default_fw_image(int product_line, const std::string& pid);
     std::vector<int> parse_fw_version(const std::string& fw);
     bool is_upgradeable(const std::string& curr, const std::string& available);
-    bool is_recommended_fw_available(std::string version);
+    bool is_recommended_fw_available(const std::string& product_line, const std::string& pid);
 
     class firmware_update_manager : public process_manager
     {
@@ -28,7 +29,7 @@ namespace rs2
         std::shared_ptr<notifications_model> get_protected_notification_model() { return _not_model.lock(); };
 
     private:
-        void process_flow(std::function<void()> cleanup, 
+        void process_flow(std::function<void()> cleanup,
             invoker invoke) override;
         bool check_for(
             std::function<bool()> action, std::function<void()> cleanup,
